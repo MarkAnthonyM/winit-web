c076f55ddd010c0b4e4d3b45d745be20967f6004
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-11-04 04:36:41 -0500

    Implement init_web trait method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+use std::rc::Rc;
+use winit;
+
+#[cfg(target_arch = "wasm32")]
+trait WinitWeb {
+    /// Add a canvas element to the HTML body and enable resize support.
+    fn init_web(self: &Rc<Self>);
+}
+
+#[cfg(target_arch = "wasm32")]
+impl WinitWeb for winit::window::Window {
+    fn init_web(self: &Rc<Self>) {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowExtWebSys;
+
+        // Attach winit canvas to body element
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(self.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+        
+        // Initialize winit window with current dimensions of browser client
+        self.set_inner_size(Self::get_client_size());
+
+        let winit_window = Rc::clone(&self);
+        let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
+            winit_window.set_inner_size(Self::get_client_size());
+        }) as Box<dyn FnMut(_)>);
+
+        // Listen for resize event on browser client. Adjust winit window dimensions on event trigger
+        let client_window = web_sys::window().unwrap();
+        client_window
+            .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+            .unwrap();
+        
+        closure.forget();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
